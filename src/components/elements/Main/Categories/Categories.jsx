5f0b0a70068cacf13362961c350e3e9934878dc7
Роман <roma39281@mail.ru>
2023-09-27 01:15:11 +0300

    added activity switching to Categories

--- src/components/elements/Main/Categories/Categories.jsx
+++ src/components/elements/Main/Categories/Categories.jsx
@@ -1,11 +1,25 @@
+import React from 'react';
 import styles from './Categories.module.scss';
 
 function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const categories = ['в километрах', 'в лунных орбитах'];
+
+  const onClickCategories = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
     <div className={styles.categories}>
       <ul>
-        <li className={styles.active}>в километрах</li>
-        <li>в лунных орбитах</li>
+        {categories.map((category, index) => (
+          <li
+            className={activeIndex === index ? styles.active : ''}
+            onClick={() => onClickCategories(index)}
+            key={`${category}_${index}`}>
+            {category}
+          </li>
+        ))}
       </ul>
     </div>
   );
